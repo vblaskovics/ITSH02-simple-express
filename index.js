a143d529d043a01139ab522076d7c955d477e0a1
Blaskovics Viktor <viktor.blaskovics@gmail.com>
2021-10-08 10:19:52 +0200

    Add VSCode docker env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 // sendFile will go here
 app.get('/', function(req, res) {
